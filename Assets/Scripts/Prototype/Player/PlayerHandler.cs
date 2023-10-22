dc50152274095f57596ce32bb65cfe964bdb574b
scarffy <34879344+scarffy@users.noreply.github.com>
2023-10-23 03:53:31 +0800

    refactor: add comment on functions

--- Assets/Scripts/Prototype/Player/PlayerHandler.cs
+++ Assets/Scripts/Prototype/Player/PlayerHandler.cs
@@ -6,27 +6,35 @@ using UnityEngine;
 
 namespace ProgrammingTask
 {
+    /// <summary>
+    /// Handles player initial spawn logic
+    /// </summary>
     public class PlayerHandler : NetworkBehaviour
     {
         [SerializeField] private PlayerMovement _playerMovement;
         
         public override void OnNetworkSpawn()
         {
+            //! Subcribe to load scene event
             NetworkManager.Singleton.SceneManager.OnSceneEvent += OnSceneEvent;
         }
         
+        /// <summary>
+        /// This function run completely on server side
+        /// </summary>
+        /// <param name="sceneEvent"></param>
         private void OnSceneEvent(SceneEvent sceneEvent)
         {
-            //! This run completely on server side
+            //! This run completely on server side (Didn't know this before)
             if (sceneEvent.SceneName == "Game" && sceneEvent.SceneEventType == SceneEventType.LoadComplete)
             {
-                Debug.Log("With scene event");
                 StartCoroutine(MovePlayerToSpawnPosition());
                 StartCoroutine(SetupPlayer());
             }
         }
 
         //! For some reason I couldn't make it move to spawn desired. I give up. 
+        //! TODO: Revisit this when I have extra extra time
         private IEnumerator MovePlayerToSpawnPosition()
         {
             yield return new WaitUntil(() => SpawnManager.Instance != null);
@@ -47,12 +55,12 @@ namespace ProgrammingTask
             Debug.Log("Moving player to random position");
         }
 
-        //! For some weird reason, this work fine
+        //! For some weird reason, this work fine but move player doesn't work.
+        //! Why? Logic error?
         private IEnumerator SetupPlayer()
         {
             yield return new WaitUntil(() => SpawnManager.Instance != null);
             SetupPlayer_ClientRpc();
-            Debug.Log("Setup player");
         }
 
         [ClientRpc]
